672ad7256abf9c357fa639aa115dde3668ec7c93
jpkneller <jpknelle@ncsu.edu>
2024-03-06 09:27:57 -0500

    Update mixing_angles.cpp

--- src/mixing_angles.cpp
+++ src/mixing_angles.cpp
@@ -28,13 +28,13 @@ MATRIX<complex<double>,NF,NF> MixingMatrix(MATRIX<complex<double>,NF,NF> Hf,arra
               den2[mu]=real(C<mu,mu>(Hf,k[j]))*d;
               den2[tau]=real(C<tau,tau>(Hf,k[j]))*d;
 
-              if(den2[e]>=den2[mu] && den2[e]>=den2[tau]){ u[e][j]=C<e,e>(Hf,k[j])/sqrt(den2[e]);       u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(den2[e]);     u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(den2[e]);}
-              if(den2[mu]>=den2[e] && den2[mu]>=den2[tau]){ u[e][j]=C<mu,e>(Hf,k[j])/sqrt(den2[mu]);    u[mu][j]=C<mu,mu>(Hf,k[j])/sqrt(den2[mu]);   u[tau][j]=C<mu,tau>(Hf,k[j])/sqrt(den2[mu]);}
-              if(den2[tau]>=den2[e] && den2[tau]>=den2[mu]){ u[e][j]=C<tau,e>(Hf,k[j])/sqrt(den2[tau]); u[mu][j]=C<tau,mu>(Hf,k[j])/sqrt(den2[tau]); u[tau][j]=C<tau,tau>(Hf,k[j])/sqrt(den2[tau]);}
+              //if(den2[e]>=den2[mu] && den2[e]>=den2[tau]){ u[e][j]=C<e,e>(Hf,k[j])/sqrt(den2[e]);       u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(den2[e]);     u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(den2[e]);}
+              //if(den2[mu]>=den2[e] && den2[mu]>=den2[tau]){ u[e][j]=C<mu,e>(Hf,k[j])/sqrt(den2[mu]);    u[mu][j]=C<mu,mu>(Hf,k[j])/sqrt(den2[mu]);   u[tau][j]=C<mu,tau>(Hf,k[j])/sqrt(den2[mu]);}
+              //if(den2[tau]>=den2[e] && den2[tau]>=den2[mu]){ u[e][j]=C<tau,e>(Hf,k[j])/sqrt(den2[tau]); u[mu][j]=C<tau,mu>(Hf,k[j])/sqrt(den2[tau]); u[tau][j]=C<tau,tau>(Hf,k[j])/sqrt(den2[tau]);}
               // set the element in the e (top) row to be pure real for the first two columns
-              //if(j==0 || j==1){ u[e][j]=C<e,e>(Hf,k[j])/sqrt(den2[e]); u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(den2[e]); u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(den2[e]);}
+              if(j==0 || j==1){ u[e][j]=C<e,e>(Hf,k[j])/sqrt(den2[e]); u[mu][j]=C<e,mu>(Hf,k[j])/sqrt(den2[e]); u[tau][j]=C<e,tau>(Hf,k[j])/sqrt(den2[e]);}
               // set the element in the tau (bottom) row to be pure real for the 3rd column
-              //if(j==2){ u[e][j]=C<tau,e>(Hf,k[j])/sqrt(den2[tau]); u[mu][j]=C<tau,mu>(Hf,k[j])/sqrt(den2[tau]); u[tau][j]=C<tau,tau>(Hf,k[j])/sqrt(den2[tau]);}
+              if(j==2){ u[e][j]=C<tau,e>(Hf,k[j])/sqrt(den2[tau]); u[mu][j]=C<tau,mu>(Hf,k[j])/sqrt(den2[tau]); u[tau][j]=C<tau,tau>(Hf,k[j])/sqrt(den2[tau]);}
             }
        
         return u;
